6bb80b2fbbd8bdc72a1d72f71bc603804ec5cb18
hardKokos <102967897+hardKokos@users.noreply.github.com>
2024-02-10 10:15:13 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "tested_declarations.h"
+#include "rdebug.h"
+#include "heap.h"
+#include "tested_declarations.h"
+#include "rdebug.h"
+
+int main(void) {
+    return 0;
+}
